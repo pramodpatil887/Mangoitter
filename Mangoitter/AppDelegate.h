7dc675652574bc32c92d66b14572f8ae62e425e9
Pramod Patil <PramodPatil@ecsion.com>
2018-01-05 16:38:38 +0530

    Initial Commit

---
+++ Mangoitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mangoitter
+//
+//  Created by Pramod Ganapati Patil on 05/01/18.
+//  Copyright © 2018 Magnoitter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mangoitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mangoitter
+//
+//  Created by Pramod Ganapati Patil on 05/01/18.
+//  Copyright © 2018 Magnoitter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
